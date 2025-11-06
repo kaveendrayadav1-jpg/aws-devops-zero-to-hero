fed18fcae493098900d4d3c4b7361ee534c8825d
kaveendrayadav1-jpg <kaveendrayadav1@gmail.com>
2025-11-06 22:04:01 +0530

    Remove aws_subnet resource from main.tf
    Removed the aws_subnet resource definition.

--- day-24/main.tf
+++ day-24/main.tf
@@ -1,8 +1,6 @@
 resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr
 }
-
-resource "aws_subnet" "sub1" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "us-east-1a"
